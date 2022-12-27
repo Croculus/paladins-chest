304fe79b07f30fce75bb597576a87e4bff8d8a8b
Croculus <awildsnappyboi@gmail.com>
2022-12-27 03:59:51 -0500

    12/26 changes
    got the loadup  and imager done in a hurry, now to get the text recognition algorithm.

--- deal_to_txt.py
+++ deal_to_txt.py
@@ -1,5 +1,7 @@
-import os
+import os, time
+from ahk.window import Window
 from ahk import AHK, ActionChain
+from PIL import ImageGrab
 
 
 ac = ActionChain()
@@ -7,17 +9,24 @@ ahk = AHK()
 
 win = ahk.active_window           
 
-run= os.startfile(r'com.epicgames.launcher://apps/badb0ee71b474ed591ec43212547cfc8%3A85a98d012e1245c8a6572f03fcf920b6%3AAntbird?action=launch&silent=true')
+#run= os.startfile(r'com.epicgames.launcher://apps/badb0ee71b474ed591ec43212547cfc8%3A85a98d012e1245c8a6572f03fcf920b6%3AAntbird?action=launch&silent=true')
 
-try: 
-    win = ahk.win_wait(title = 'Paladins (64-bit, DX11)', timeout=40)#doesn't work idk why
+print('y')
+ss_region = (172, 185, 1251, 651)
+ss_img = ImageGrab.grab(ss_region)
+ss_img.save("image\deal.jpg")
+'''
+try:
+    win = ahk.win_wait(title = 'Paladins (64-bit', timeout=40)#doesn't work idk why
     win.activate()#doesn't work idk why
-    ac.sleep(15)
+    print('yes')
+    ac.sleep(45)
     ac.click(122, 443)
     ac.sleep(3)
     ac.click(781, 76)
-    print('yes')
+    ac.perform()
+
     
 except TimeoutError:
-    print("No Paladins")
-
+    print('Notepad was not found!')
+'''
